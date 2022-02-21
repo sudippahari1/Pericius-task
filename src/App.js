2d09ccc467359a6f8fb4ed8cdd7dc5e8fdff6b35
Sudip Pahari <sudippahari1@gmail.com>
2022-02-21 16:39:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      newItem:'',
+      list : []
+    };
+  }
+
+addItem(nameValue){
+  if(nameValue !== ''){
+    const newItem ={
+      id: Date.now(),
+      value: nameValue,
+      isDone: false
+    }
+    const list =[...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem:''
+    });
+ }
+}
+
+updateInput(input){
+  this.setState({newItem:input})
+}
+render(){
+    return(
+      <div className='app-title'>
+      <h1 >  Name Add</h1>
+        
+        <div className='container1' >
+       <input 
+          type="text" 
+          className='input-text'
+          placeholder='Place Enter Name' 
+          required
+          value={this.state.newItem}
+          onChange={e=> this.updateInput(e.target.value)}
+            />
+            <button 
+            className='add-btn'
+            onClick={()=> this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+            >enter
+            </button>
+            </div>
+            <div className="list">
+              <ul>
+              {this.state.list.map(item=>{
+                return(
+                  <div>
+                  <li key={item.id}>
+               
+                    {item.value}
+                    
+                  </li>
+                  </div>
+                )
+              })}
+               
+              </ul>
+            </div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
